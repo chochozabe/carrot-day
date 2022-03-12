d7be4eddef5f676638415f6a55fc32b942c7350b
chochozabe <cintamani_whyk@naver.com>
2022-03-13 04:09:46 +0900

    Chore : 불필요한 파일, 코드 삭제

---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <div className="Header-title">Have a good carrot day🥕</div>
+      <span className="Header-subtitle">
+        열심히 일한 당신에게, 채찍이 아닌 당근을 주는 날
+      </span>
+    </div>
+  );
+}
